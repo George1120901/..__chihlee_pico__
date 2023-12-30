b2cd58d12ca76b4c08852851390d2a31b0fe2ba3
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-30 14:55:03 +0800

    modify

--- project1/fastAPI/index.py
+++ project1/fastAPI/index.py
@@ -1,1 +1,24 @@
-print("Hello! World!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    date:str
+    distance:float
+    light:float
+
+app = FastAPI()
+
+@app.post("/items/")
+async def update_item(item:Item):
+    print(item)
+    return {'status':'ok'}
+
+@app.get("/items/{date}/{distance}/{light}")
+async def update_item1(date:str,distance:float,light:float):
+    data = {
+        'date' : date,
+        'distance':distance,
+        'light':light
+    }
+    print(data)
+    return data
