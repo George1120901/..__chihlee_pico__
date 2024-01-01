46f843907043aba1ebee8780245888a75fcba678
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-01 11:40:24 +0800

    modify

--- project1/fastAPI/index.py
+++ project1/fastAPI/index.py
@@ -21,7 +21,7 @@ async def update_item(item:Item):
 
 @app.get("/items/{date}/{distance}/{light}")
 async def update_item1(date:str,distance:float,light:float):
-    
+    #lpush是放在redis list的最前方
     renderRedis.lpush('pico_distance',distance)
     renderRedis.lpush('pico_light',light)
     
@@ -29,9 +29,17 @@ async def update_item1(date:str,distance:float,light:float):
 
 @app.post("/items/{lastNum}")
 async def get_items(lastNum:int):
-    #redis取出資料  
+    #redis取出最前方資料(因為更新在最前方)
     distances = renderRedis.lrange('pico_distance',0,lastNum)
     light = renderRedis.lrange('pico_light',0,lastNum)
+    
+    #傳出的資料的索引是[9,8,7,6,5,4,3,2,1,0],所以要反轉資料
+    #0是每次更新的最新資料,
+    #list.reverse(),傳出None
+    
+    distances.reverse()
+    light.reverse()
+
 
     #將byte string 轉換為 str
     distances_list = [item.decode('utf-8') for item in distances]
