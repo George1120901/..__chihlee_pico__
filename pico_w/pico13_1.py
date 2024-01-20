d0b31780f297eba497df1a9063526a827f405a8e
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-20 11:46:03 +0800

    Update pico13_1.py

--- pico_w/pico13_1.py
+++ pico_w/pico13_1.py
@@ -1,3 +1,13 @@
 from machine import Pin
+import time
+
 red_led = Pin(15,mode=Pin.OUT)
-red_led.value(0)
+btn = Pin(14,mode=Pin.PULL_DOWN)
+
+while True:
+    if btn.value():
+        red_led.value(1)
+    else:
+        red_led.value(0)
+    
+    
