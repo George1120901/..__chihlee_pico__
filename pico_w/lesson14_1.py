9d4560f48892512e80afa82530154ad156360a89
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-27 15:41:07 +0800

    Update lesson14_1.py

--- pico_w/lesson14_1.py
+++ pico_w/lesson14_1.py
@@ -1,16 +1,33 @@
 from machine import Timer,Pin,ADC
 import time
+from tools import connect,reconnect
+import urequests as requests
 
 
 
 def fun10(t:Timer | None = None):
-    print('10秒了')
-    led.toggle()
+    light_value = light.read_u16()
+    vr_value = vr.read_u16()
+    url = f'https://blynk.cloud/external/api/batch/update?token=自已的token={light_value}&v1={vr_value}'
+    try:
+        led.value(1)
+        response = requests.get(url)
+    except:
+        reconnect()
+    else:
+        if response.status_code == 200:
+            print("傳送成功")
+        else:
+            print("傳送失敗")
+        response.close()
+    led.value(0)    
+    
 
 def fun500ms(t:Timer):
     print(f'light:{light.read_u16()}')
     print(f'vr:{vr.read_u16()}')
-    
+
+connect()
 led = Pin(15, Pin.OUT)
 light = ADC(Pin(28))
 vr = ADC(Pin(27))
