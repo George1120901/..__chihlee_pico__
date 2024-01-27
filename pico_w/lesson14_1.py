21ad5f66438281bb44969e228546d46c84b5df6c
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-27 14:37:13 +0800

    Update lesson14_1.py

--- pico_w/lesson14_1.py
+++ pico_w/lesson14_1.py
@@ -1,17 +1,18 @@
 from machine import Timer,Pin,ADC
+import time
+
 
 
 def fun10(t:Timer | None = None):
     print('10秒了')
     led.toggle()
+
+def fun500ms(t:Timer):
+    print(light.read_u16())
     
 led = Pin(15, Pin.OUT)
 light = ADC(Pin(28))
 timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10)
+timer500ms = Timer(period=500, mode=Timer.PERIODIC, callback=fun500ms)
 fun10()
 
-
-
-
-while True:
-    print(light.read_u16())
